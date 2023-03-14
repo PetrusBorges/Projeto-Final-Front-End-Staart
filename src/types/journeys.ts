8d1b19893d7e50ba769f940c3bcd67534a491d6c
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-03-14 13:05:20 -0300

    feat: add Journeys type

---
+++ src/types/journeys.ts
@@ -0,0 +1,13 @@
+export interface JourneysProps {
+  medias: {
+    thumb: string;
+    poster: string;
+    banner: string;
+  },
+  description: string;
+  group: string;
+  coursesID: [],
+  title: string;
+  pathID: string;
+  slug: string;
+}
