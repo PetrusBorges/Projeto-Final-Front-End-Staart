8c73ef8f8f8de06c7e06f3f650b9feb27a7fddb4
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-03-14 12:51:31 -0300

    feat: add component Loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,22 @@
+import { Overlay } from './styles';
+
+import { ReactPortal } from '../ReactPortal';
+import { Spinner } from '../Spinner';
+
+interface LoaderProps {
+  isLoading: boolean;
+}
+
+export function Loader({ isLoading }: LoaderProps) {
+  if(!isLoading) {
+    return null;
+  }
+
+  return (
+    <ReactPortal containerId='loader-root'>
+      <Overlay>
+        <Spinner size={90}/>
+      </Overlay>
+    </ReactPortal>
+  );
+}
---
+++ src/components/Loader/styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.div`
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: fixed;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(13, 17, 24, 0.7);
+`;
