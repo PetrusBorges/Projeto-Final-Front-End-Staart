99087eeae5906cb9e3a273bf8fbabf3dddd77f08
Petrus Borges Machado <contatopetrusborges@gmail.com>
2023-03-14 13:01:31 -0300

    feat: add ForgetPassword page

---
+++ src/pages/ForgetPassword/index.tsx
@@ -0,0 +1,48 @@
+import { Container, Content, Form, ButtonContainer, Footer} from './styles';
+
+import { FormGroup } from '../../components/FormGroup';
+import { Input } from '../../components/input';
+import { Button } from '../../components/Button';
+
+import { Link } from 'react-router-dom';
+
+import { useForgetPassword } from '../../hooks/useForgetPassword';
+
+export function ForgotPassword() {
+
+  const { isLoading, handleSubmit, handleEmailChange, getErrorMessageByFieldName, isEmailOk, email } = useForgetPassword();
+
+  return (
+    <Container>
+      <Content>
+        <h1>Recuperar senha</h1>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup error={getErrorMessageByFieldName('email')}>
+            <Input
+              value={email}
+              type='email'
+              placeholder='Email'
+              onChange={handleEmailChange}
+              error={getErrorMessageByFieldName('email')}
+            />
+          </FormGroup>
+
+          <ButtonContainer>
+            <Button
+              type='submit'
+              disabled={!isEmailOk}
+              isLoading={isLoading}
+            >
+              Recuperar senha
+            </Button>
+          </ButtonContainer>
+        </Form>
+
+        <Footer>
+          <Link to='/signup'>Não tenho uma conta</Link>
+          <Link to='/'>Fazer Login</Link>
+        </Footer>
+      </Content>
+    </Container>
+  );
+}
